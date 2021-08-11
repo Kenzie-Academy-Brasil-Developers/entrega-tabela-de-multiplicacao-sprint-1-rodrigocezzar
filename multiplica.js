f3c354f00975d4d8b757c598c2a94b6463721c0d
rodrigocezzar <rodrigocezzar85@gmail.com>
2021-08-11 19:16:52 -0300

    resolução_rodrigo_abreu

---
+++ multiplica.js
@@ -0,0 +1,11 @@
+function matriz(n) {
+    let x = [];
+    for (let i = 0; i <= n; i++) {
+            x[i] = [];
+        for (let j = 0; j <= n; j++) {
+            x[i][j] = ((i*j));
+        }       
+    }
+    return x   
+}
+    console.table(matriz(10));
