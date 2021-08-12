cd505006162bd34c969ccba86dea700863bdf504
rodrigocezzar <rodrigocezzar85@gmail.com>
2021-08-12 15:22:34 -0300

    correção_rodrigo_abreu

--- multiplica.js
+++ multiplica.js
@@ -3,9 +3,12 @@ function matriz(n) {
     for (let i = 0; i <= n; i++) {
             x[i] = [];
         for (let j = 0; j <= n; j++) {
-            x[i][j] = ((i*j));
-        }       
-    }
-    return x   
+            x[i][j] = (i*j);            
+        }         
+            
+    }   
+        return x          
 }
-    console.table(matriz(10));
+
+console.table(matriz(10));
+   
